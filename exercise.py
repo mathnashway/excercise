020b4570eba07dede9bdd5441407efd434925211
nashway <mathnashway@gmail.com>
2019-12-30 00:44:26 +0800

    最終版

--- exercise.py
+++ exercise.py
@@ -1,10 +1,14 @@
 password = 'a123456'
 i = 0
 while i < 3 :
+	i = i + 1
 	psd = input('請輸入密碼: ')
 	if psd == password :
 		print('恭喜正確')
 		break
 	else :
-		i = i + 1
-		print('輸入第', i , '次錯誤 ,還有', 3-i ,'次機會')
+		print('輸入第', i , '次錯誤')
+		if i < 3:
+			print('還有', 3-i ,'次機會')
+		else:
+			print('鎖住了')
